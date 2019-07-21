4b9838d587b2f6e24432a503e3ff89869aa21b98
h4x00r <50273964+4zer0day@users.noreply.github.com>
2019-07-22 02:29:39 +0800

    Add files via upload

---
+++ monotone.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+clear
+RED='\033[0;37m'
+NC='\033[0m'
+echo "" 
+echo ""  
+COLUMNS=$(tput cols) 
+title="TIME IS WASTED, HAPPINESS IS TEMPORARY, YOU'RE STILL A TRASH, GIRL"  
+printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+	         
+echo -e ""
+echo -e ""  
+	                                                                                                
+COLUMNS=$(tput cols) 
+title="Press Space Bar to Read this intriguing story..."
+printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+read -n 1
+clear
+echo "1/6/2018/5/2019"
+read -n 1
+clear
+echo -e "Now you don't even answer the phone"
+read -n 1
+clear
+echo -e "I hear that ${RED}monotone ${NC}voice instead of you"
+read -n 1
+clear
+echo "I know there are hard times in life"
+read -n 1
+clear
+echo "You couldn't overcome that little moment and went away for an alternative"
+read -n 1
+clear
+echo "Let's take a break (That's what you said)"
+read -n 1
+clear
+echo "I took those words as they were"
+read -n 1
+clear
+echo "All we need is time"
+read -n 1
+clear
+echo "That's the way I understood"
+read -n 1
+clear
+echo "But...You"
+read -n 1
+clear
+echo "How could you do that, you're unbelievable"
+read -n 1
+clear
+echo "Like nothing happened, you broke my heart"
+read -n 1
+clear
+echo "Your smiling face shows that you've got over me"
+read -n 1
+clear
+echo "You said let's take a break yea"
+read -n 1
+clear
+echo "Trusted you and now what you've done this to me"
+read -n 1
+clear
+echo "and you look happy"
+read -n 1
+clear
+echo "Is flirting with plenty of guys make you forget everything about me?"
+read -n 1
+clear
+echo "Well yeah as long as you're happy, I wouldn't tell that typical lie"
+read -n 1
+clear
+echo "Why should I wish happiness for you who left me, I don't give a f"
+read -n 1
+clear
+echo "It didn't take that long for you since you're a charmer (I won't say that word)"
+read -n 1
+clear
+echo "I see your photos online, are you that happy? you can't stop smiling"
+read -n 1
+clear
+echo "I still have heartache when I breathe"
+read -n 1
+clear
+echo "Lucky that it won't happen to you"
+read -n 1
+clear
+echo "If you break up again, All you have to do is to find another love (you had all of that guy while you're with me)"
+read -n 1
+clear
+echo "I know it's the end, you'll never ever come back to me, I don't expect that"
+read -n 1
+clear
+echo "Didn't know how much you're better off you'll be without me uh "
+read -n 1
+clear
+echo "You said let's take a break, we need time and that was it.. I know.."
+read -n 1
+clear
+echo "You left me waiting.. and all at once"
+read -n 1
+clear
+echo "You left me, and met that other guy"
+read -n 1
+clear
+echo "As if I was never there"
+read -n 1
+clear
+echo "You fell in love"
+read -n 1
+clear
+echo "" 
+echo "" 
+COLUMNS=$(tput cols) 
+title="THE END"
+printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+
+
+
+
+
